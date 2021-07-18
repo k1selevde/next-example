ff2f6a32a4d8040cdc2d1de07e5c119a466b7d46
Denis <k1selevde@yandex.ru>
2021-07-18 15:35:02 +0500

    queries to api

--- pages/users.js
+++ pages/users.js
@@ -1,11 +1,8 @@
 import Link from "next/link"
-import {useState} from "react"
+import {useState, useEffect} from "react"
 
-const Users = () => {
-    const [users, setUsers] = useState([
-        {id: 1, name: 'petr'},
-        {id: 2, name: 'vasya'},
-    ])
+const Users = ({users}) => {
+
     return (
         <div>
             <div>
@@ -27,3 +24,11 @@ const Users = () => {
 };
 
 export default Users;
+
+export async function getStaticProps(context) {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`)
+    const users = await response.json()
+    return {
+        props: {users}, // will be passed to the page component as props
+    }
+}
--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,11 +1,22 @@
 import {useRouter} from "next/router";
+import styles from "../../styles/User.module.scss";
 
-export default function () {
+export default function User({user}) {
     const {query: { id }} = useRouter()
 
     return (
-        <div>
+        <div className={styles.user}>
             <h1>Пользователь с id {id}</h1>
+            <div>Name: {user.name}</div>
         </div>
     )
+}
+
+export async function getServerSideProps({params: {id}}) {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    const user = await response.json()
+
+    return {
+        props: {user}
+    }
 }
